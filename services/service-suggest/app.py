9c0616e87ebcf48a4ab4c7543abecd6e13afba09
lehuy1121 <nguyenlehuy9000@gmail.com>
2024-11-26 22:35:56 +0700

    update bug

--- services/service-suggest/app.py
+++ services/service-suggest/app.py
@@ -12,9 +12,10 @@ from flask_cors import CORS
 load_dotenv()
 # Kết nối Redis
 r = redis.StrictRedis(host='redis', port=6379, db=0)
+app = Flask(__name__)
 # Cấu hình CORS cho toàn bộ ứng dụng và cho phép tất cả các nguồn
 CORS(app, resources={r"/*": {"origins": "*"}})
-app = Flask(__name__)
+
 # Worker chạy trong nền để nhận và xử lý task
 def worker():
     while True:
