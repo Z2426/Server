254f0e12e460f68f59b56df206cd9a04b1f78151
lehuy1121 <nguyenlehuy9000@gmail.com>
2024-11-26 21:36:17 +0700

    t1

--- services/moderationAi-service/app.py
+++ services/moderationAi-service/app.py
@@ -3,7 +3,8 @@ from controllers.content_controller import content_bp
 from flask_cors import CORS
 app = Flask(__name__)
 # Cấu hình CORS cho toàn bộ ứng dụng và cho phép tất cả các nguồn
-CORS(app, resources={r"/*": {"origins": "*"}})
+# Cấu hình CORS cho phép frontend từ localhost:3006
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 # Đăng ký blueprint cho content
 app.register_blueprint(content_bp)
 
