760b0f330a19fde97e5b1558ca44de355af16973
lehuy1121 <nguyenlehuy9000@gmail.com>
2024-12-05 12:02:28 +0700

    update code

--- services/HAL9000/app.js
+++ services/HAL9000/app.js
@@ -17,61 +17,6 @@ app.use(cors(corsOptions));
 app.use('/', AiRoutes);
 app.use(errorHandler)
 
-// Hàm xử lý dữ liệu đầu vào, chỉ lấy các key cần thiết
-const processInputData = (inputData, allowedKeys) => {
-    const criteria = {};
-
-    // Ánh xạ các key từ dữ liệu đầu vào sang key sử dụng trong query
-    const fieldMappings = {
-        "ADDRESS:ADDRESS": "address",
-        "AGE:AGE": "age",
-        "HOBBY:HOBBY": "hobby",
-        "NAME:NAME": "name",
-        "Province:Province": "province",
-        "SCHOOL:SCHOOL": "school",
-        "WORKPLACE:WORKPLACE": "workplace"
-    };
-
-    // Duyệt qua tất cả các key trong inputData
-    for (const key in inputData) {
-        // Kiểm tra key có trong allowedKeys và fieldMappings
-        if (allowedKeys.includes(fieldMappings[key]) && inputData[key]) {
-            criteria[fieldMappings[key]] = inputData[key]; // Thêm vào criteria nếu key hợp lệ và có giá trị
-        }
-    }
-
-    return criteria;
-};
-
-// Hàm xử lý tìm kiếm người dùng, chỉ lấy các key cần thiết từ inputData
-const processInputFindUser = (inputData) => {
-    const criteria = {};
-
-    // Các key cần lọc
-    const allowedKeys = ['age', 'name', 'workplace', 'hobby'];
-
-    // Ánh xạ các key từ dữ liệu đầu vào sang key sử dụng trong query
-    const fieldMappings = {
-        "ADDRESS:ADDRESS": "address",
-        "AGE:AGE": "age",
-        "HOBBY:HOBBY": "hobby",
-        "NAME:NAME": "name",
-        "Province:Province": "province",
-        "SCHOOL:SCHOOL": "school",
-        "WORKPLACE:WORKPLACE": "workplace"
-    };
-
-    // Duyệt qua tất cả các key trong inputData
-    for (const key in inputData) {
-        // Kiểm tra key có trong allowedKeys và fieldMappings
-        if (allowedKeys.includes(fieldMappings[key]) && inputData[key]) {
-            criteria[fieldMappings[key]] = inputData[key]; // Thêm vào criteria nếu key hợp lệ và có giá trị
-        }
-    }
-
-    return criteria;
-};
-
 const PORT = process.env.BOT_PORT || 4000;
 app.listen(PORT, () => {
     console.log(`User service running on port ${PORT}`);
